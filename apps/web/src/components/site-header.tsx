5078533a4c1827b9af6021bfbce896850ca62101
Rob Park <rbbpark@gmail.com>
2025-03-28 23:52:12 -0700

    make sidebar icon filters

--- apps/web/src/components/site-header.tsx
+++ apps/web/src/components/site-header.tsx
@@ -1,13 +1,16 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Input } from "@/components/ui/input";
+import { SlidersHorizontal } from "lucide-react";
 
 export function SiteHeader() {
   return (
     <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <h1 className="text-base font-medium">Retrolist</h1>
-        <SidebarTrigger className="-mr-1 ml-auto rotate-180" />
+        <SidebarTrigger className="-mr-1 ml-auto rotate-180">
+          <SlidersHorizontal />
+        </SidebarTrigger>
       </div>
     </header>
   );
